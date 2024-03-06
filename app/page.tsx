30c45c0adc0e78f91b4742eb233d692ba3a2fcf0
920 <920.anish920@gmail.com>
2024-03-06 15:22:16 +0545

    forgor

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
-import PlayingAnimation from "./assets/animation";
-import SpotifyLogo from "./assets/logo";
+'use client';
 import Socials from "./socials";
 import Image from "next/image";
 import avatar from "./assets/avatar.png";
-
-
-
-
+import SpotifyContainer from "./playing";
 
 
 export default function Home() {
@@ -16,13 +12,15 @@ export default function Home() {
         <div className="flex flex-col gap-2 items-center p-12 sm:p-20 rounded-lg shadow-lg hover:shadow-2xl dark:hover:shadow-slate-800 transition-shadow duration-500 ease-in-out cursor-pointer bg-slate-100 dark:bg-zinc-900">
           <h1 className="text-5xl sm:text-6xl md:text-7xl font-medium text-center mb-4">920</h1>
           <div className="avatar-container mb-6">
-            <Image src={avatar} alt="Avatar" className="avatar-img rounded-full" width={100} height={100} />
+            <Image priority src={avatar} alt="Avatar" className="avatar-img rounded-full grayscale hover:grayscale-0" width={100} height={100} />
           </div>
           <Socials />
-          <SpotifyLogo />
-          <PlayingAnimation />
+          <SpotifyContainer />
         </div>
       </div>
     </main>
   );
 }
+
+
+
---
+++ app/playing.tsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from 'react';
+import PlayingAnimation from './assets/animation';
+import SpotifyLogo from "./assets/logo";
+
+interface NowPlayingData {
+  isOnline: boolean;
+  song: string;
+  artist: string;
+  link: string;
+  image: string;
+}
+
+const SpotifyContainer = () => {
+  const [nowPlayingData, setNowPlayingData] = useState<NowPlayingData>({
+    isOnline: false,
+    song: '',
+    artist: '',
+    link: '',
+    image: '',
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/spotify');
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        if (!data) {
+          console.error('Unexpected end of JSON input');
+          setNowPlayingData({ ...nowPlayingData, isOnline: false });
+        } else {
+          setNowPlayingData(data);
+        }
+      } catch (error:any) {
+        console.error('Error fetching now playing data:', error.message);
+        setNowPlayingData({ ...nowPlayingData, isOnline: false });
+      }
+    };
+
+    const interval = setInterval(fetchData, 10000);
+
+    fetchData();
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="mt-8 text-center">
+      <div className="max-w-sm mx-auto flex flex-col items-center justify-center mb-4">
+        <SpotifyLogo />
+        <div className="flex justify-center mt-2">
+          {nowPlayingData.isOnline ? (
+            <span className="bg-green-300 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">ONLINE</span>
+          ) : (
+            <span className="bg-gray-300 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">OFFLINE</span>
+          )}
+        </div>
+        {nowPlayingData.isOnline && <PlayingAnimation />}
+        {nowPlayingData.isOnline ? (
+          <div className="max-w-sm mx-auto m-4">
+            <img src={nowPlayingData.image} height={100} width={100} alt="Album Art" className="rounded-lg mb-4 mx-auto" />
+            <div className="text-center mb-4">
+              <h2 className="text-sm font-semibold">{nowPlayingData.song}</h2>
+              <p className="text-gray-600">
+                <a href={nowPlayingData.link} target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:underline">
+                  {nowPlayingData.artist}
+                </a>
+              </p>
+            </div>
+          </div>
+        ) : (
+          <p className="text-gray-600 m-4">Listenin' to the voices!</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SpotifyContainer;
