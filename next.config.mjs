09af1181f365b73b5127b18f6b356075642be63a
920 <920.anish920@gmail.com>
2024-03-06 16:49:35 +0545

    Added remotePatterns for images in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'i.scdn.co',
+          pathname: '/**', 
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
