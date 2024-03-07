6a0318c991c51e7e6a5ff46bd3ad77e4447da9c2
920 <920.anish920@gmail.com>
2024-03-07 19:07:28 +0545

    adjusted spacing

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center"> 
       <div className="flex flex-col gap-2 items-center justify-center h-screen">
-        <div className="flex flex-col gap-2 items-center px-16 py-6 sm:py-10 rounded-lg shadow-lg hover:shadow-2xl dark:hover:shadow-slate-800 transition-shadow duration-500 ease-in-out cursor-pointer bg-slate-100 dark:bg-zinc-900">
+        <div className="flex flex-col gap-2 items-center px-16 py-6 sm:py-16 rounded-lg shadow-lg hover:shadow-2xl dark:hover:shadow-slate-800 transition-shadow duration-500 ease-in-out cursor-pointer bg-slate-100 dark:bg-zinc-900">
           <h1 className="text-5xl sm:text-6xl md:text-7xl font-medium text-center">920</h1>
           <div className="avatar-container">
             <Image priority src={avatar} alt="Avatar" className="avatar-img rounded-full grayscale hover:grayscale-0" width={100} height={100} />
