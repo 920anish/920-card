0c975e48e32ae54972d245e32bea71c46f92f75a
920 <anish1.775421@memc.tu.edu.np>
2026-02-04 19:50:34 +0545

    update og

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,26 @@
 import type { Metadata } from "next";
 import { Rubik } from "next/font/google";
 import "./globals.css";
+import avatar from "./assets/avatar.png";
 
 const inter = Rubik({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Social Card",
+  title: "Socials Card",
   description: "Nothing ever happens here.",
   keywords: ["920", "anish", "card", "site", "status"],
-  
-
+  openGraph: {
+    title: "Socials Card",
+    description: "Nothing ever happens here.",
+    type: "website",
+    images: [avatar.src],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Socials Card",
+    description: "Nothing ever happens here.",
+    images: avatar.src,
+  },
 };
 
 export default function RootLayout({
