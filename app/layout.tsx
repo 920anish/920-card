49204fa74c4154949419273156bba669c165334d
920 <anish1.775421@memc.tu.edu.np>
2026-02-04 20:17:00 +0545

    meh

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Rubik } from "next/font/google";
 import "./globals.css";
-import avatar from "./assets/avatar.png";
 
 const inter = Rubik({ subsets: ["latin"] });
 
@@ -9,25 +8,30 @@ export const metadata: Metadata = {
   title: "Socials Card",
   description: "Nothing ever happens here.",
   keywords: ["920", "anish", "card", "site", "status"],
+  metadataBase: new URL('https://920-card.anish920.workers.dev'), 
   openGraph: {
     title: "Socials Card",
     description: "Nothing ever happens here.",
     type: "website",
-    images: [avatar.src],
+    url: 'https://920-card.anish920.workers.dev',
+    images: [
+      {
+        url: 'app/assets/avatar.png', // Will be resolved with metadataBase
+        width: 1200,
+        height: 630,
+        alt: 'Socials Card',
+      }
+    ],
   },
   twitter: {
     card: "summary_large_image",
     title: "Socials Card",
     description: "Nothing ever happens here.",
-    images: avatar.src,
+    images: ['app/assets/avatar.png'],
   },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
